6e22a4d904ce6bcb089398cac37f77cf4e3d11d8
Mohammad Shafi <shafi.bahrami.2015@gmail.com>
2022-11-27 16:04:59 +0100

    Pagination and Bootstrap added

--- src/App.js
+++ src/App.js
@@ -1,9 +1,30 @@
 import React from 'react';
+import ReactPaginate from 'react-paginate';
 
 const App=()=>{
+
+  const handlePageClick=(data)=>{
+    console.log(data.selected);
+  }
+
    return (
      <div>
-        App
+        <ReactPaginate
+           previousLabel={"Previous"}
+           nextLabel={"Next"}
+           breakLabel={"---"}
+           pageCount={15}
+           marginPagesDisplayed={3}
+           pageRangeDisplayed={4}
+           onPageChange={handlePageClick}
+           containerClassName={"pagination"}
+           pageClassName={"page-item"}
+           pageLinkClassName={"page-link"}
+           previousClassName={"page-item"}
+           previousLinkClassName={"page-link"}
+           nextClassName={"page-item"}
+           nextLinkClassName={"page-link"}
+        />
      </div>
    )
 }
